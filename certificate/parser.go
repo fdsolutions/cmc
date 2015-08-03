8f1ad1c277ccda9bcc39d9d16260df0664cdeb2e
first-developer <lioninho11@hotmail.fr>
2015-08-04 00:42:34 +0200

    Extract test samples in its own pkg

--- certificate/parser.go
+++ certificate/parser.go
@@ -6,16 +6,28 @@ import (
 	"fmt"
 )
 
+// ErrorRef is used to keep track of PEM data that failed to be parsed
+type ErrorRef map[int]error
+
+// NewErrorRef is the constructor
+func NewErrorRef() ErrorRef {
+	return ErrorRef{}
+}
+
+// Add save the error happened when parse a PEM in 'idx' position
+func (er ErrorRef) Add(idx int, err error) {
+	er[idx] = err
+}
+
 // Parse extract all certificates from a content that list all PEM data string
-func Parse(PEMdata string) (certs []*x509.Certificate, e error) {
-	errRefs := make(map[int]error)
+func Parse(PEMdata string) (certs []*x509.Certificate, errRefs ErrorRef) {
+	errRefs = NewErrorRef()
 	_, blocks, _ := decodePEMdataInBlocks(0, []byte{}, []byte(PEMdata), errRefs)
-	fmt.Printf("%#v", errRefs)
 	certs, _ = x509.ParseCertificates(blocks)
 	return
 }
 
-func decodePEMdataInBlocks(idx int, blocks, rest []byte, errRefs map[int]error) (nextIdx int, accBlocks, next []byte) {
+func decodePEMdataInBlocks(idx int, blocks, rest []byte, errRefs ErrorRef) (nextIdx int, accBlocks, next []byte) {
 	var block *pem.Block
 	// preset return values
 	nextIdx, accBlocks, next = idx, blocks, nil
@@ -25,7 +37,7 @@ func decodePEMdataInBlocks(idx int, blocks, rest []byte, errRefs map[int]error) 
 	}
 
 	if block, next = pem.Decode(rest); block == nil {
-		errRefs[idx] = fmt.Errorf("Error: Invalid PEM content at position %v", idx)
+		errRefs.Add(idx, fmt.Errorf("Error: Invalid PEM content at position %v", idx))
 		return
 	}
 
